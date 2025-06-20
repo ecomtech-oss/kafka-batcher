032cd203ceca8e51e63b1e170a1107b8b197151b
Gleb Ivanov <glivanov@samokat.ru>
2025-06-20 14:42:52 +0300

    Improve Accumulator and Collector fault tolerance

--- lib/kafka_batcher/accumulator.ex
+++ lib/kafka_batcher/accumulator.ex
@@ -5,10 +5,11 @@ defmodule KafkaBatcher.Accumulator do
   See details how it works in KafkaBatcher.Accumulator.State module
   """
 
-  require Logger
-
   alias KafkaBatcher.{Accumulator.State, MessageObject, TempStorage}
   alias KafkaBatcher.Behaviours.Collector, as: CollectorBehaviour
+  alias KafkaBatcher.Behaviours.Accumulator, as: AccumulatorBehaviour
+
+  @behaviour AccumulatorBehaviour
 
   @error_notifier Application.compile_env(:kafka_batcher, :error_notifier, KafkaBatcher.DefaultErrorNotifier)
   @producer Application.compile_env(:kafka_batcher, :producer_module, KafkaBatcher.Producers.Kaffe)
@@ -16,11 +17,13 @@ defmodule KafkaBatcher.Accumulator do
   use GenServer
   require Logger
 
+  @impl AccumulatorBehaviour
   def start_link(args) do
     GenServer.start_link(__MODULE__, args, name: reg_name(args))
   end
 
   @doc "Returns a specification to start this module under a supervisor"
+  @impl AccumulatorBehaviour
   def child_spec(args) do
     %{
       id: reg_name(args),
@@ -31,6 +34,7 @@ defmodule KafkaBatcher.Accumulator do
   @doc """
   Finds appropriate Accumulator process by topic & partition and dispatches `event` to it
   """
+  @impl AccumulatorBehaviour
   def add_event(%MessageObject{} = event, topic_name, partition \\ nil) do
     GenServer.call(reg_name(topic_name: topic_name, partition: partition), {:add_event, event})
   end
---
+++ lib/kafka_batcher/behaviours/accumulator.ex
@@ -0,0 +1,17 @@
+defmodule KafkaBatcher.Behaviours.Accumulator do
+  @moduledoc """
+    Receives individual messages from the collector implementation
+    Sends them in batches to kafka using the configured producer
+    Applies backpressure to the collector
+  """
+
+  alias KafkaBatcher.MessageObject
+
+  @typep topic_name :: String.t()
+  @typep partition :: non_neg_integer() | nil
+
+  @callback start_link(Keyword.t()) :: GenServer.on_start()
+  @callback child_spec(Keyword.t()) :: Supervisor.child_spec()
+  @callback add_event(MessageObject.t(), topic_name()) :: term()
+  @callback add_event(MessageObject.t(), topic_name(), partition()) :: term()
+end
